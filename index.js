823eac22cd1ac7bc65867ece2e23b748cb0ec161
alamin500 <nalamin500@gmail.com>
2021-10-26 17:05:45 +0600

    h

--- index.js
+++ index.js
@@ -84,6 +84,13 @@ async function run() {
         products,
       });
     });
+    // use Post to get data by keys
+    app.post("/products/bykeys", async (req, res) => {
+      const keys = req.body;
+      const query = { key: { $in: keys } };
+      const products = await productsCollection.find(query).toArray();
+      res.json(products);
+    });
   } finally {
     // await client.close()
   }
