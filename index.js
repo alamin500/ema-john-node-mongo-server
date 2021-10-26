442d553b26bf2248b735345cdf7fbd631617d031
alamin500 <nalamin500@gmail.com>
2021-10-26 15:07:55 +0600

    h

--- index.js
+++ index.js
@@ -66,8 +66,19 @@ async function run() {
     // Get product api
     app.get("/products", async (req, res) => {
       const cursor = productsCollection.find({});
-      const products = await cursor.toArray();
+      const page = req.query.page;
+      const size = parseInt(req.query.size);
+      let products;
       const count = await cursor.count();
+      if (page) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+
       res.send({
         count,
         products,
